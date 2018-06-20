245de9c686d0626e1b514648acceddffe218f1a5
Suhel Hammoud <suhel.hammoud@gmail.com>
2018-06-20 23:07:44 +0300

    large font

--- app/src/main/java/sy/edu/au/nodemcu/F3Buttons.java
+++ app/src/main/java/sy/edu/au/nodemcu/F3Buttons.java
@@ -7,16 +7,26 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class F3Buttons extends Fragment implements View.OnTouchListener {
 
+    View rootView;
+
+    public final static int COLOR_PRESSED = 0xFFf48042;
+    public final static int COLOR_RELEASED = 0xFFbdede1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.f3_buttons, container, false);
+        rootView = inflater.inflate(R.layout.f3_buttons, container, false);
 
+        setTouchListeners();
+
+        resetButtonsColors();
+        return rootView;
+    }
+
+    private void setTouchListeners() {
         rootView.findViewById(R.id.btnStop)
                 .setOnTouchListener(this);
 
@@ -31,11 +41,25 @@ public class F3Buttons extends Fragment implements View.OnTouchListener {
 
         rootView.findViewById(R.id.btnRight)
                 .setOnTouchListener(this);
-
-
-        return rootView;
     }
 
+    private void resetButtonsColors() {
+        rootView.findViewById(R.id.btnStop)
+                .setBackgroundColor(COLOR_RELEASED);
+
+        rootView.findViewById(R.id.btnForward)
+                .setBackgroundColor(COLOR_RELEASED);
+
+        rootView.findViewById(R.id.btnBackword)
+                .setBackgroundColor(COLOR_RELEASED);
+
+        rootView.findViewById(R.id.btnLeft)
+                .setBackgroundColor(COLOR_RELEASED);
+
+        rootView.findViewById(R.id.btnRight)
+                .setBackgroundColor(COLOR_RELEASED);
+
+    }
 
     public static F3Buttons of(int sectionNumber) {
         Log.d("suhel", "F3Buttons of " + sectionNumber);
@@ -73,16 +97,20 @@ public class F3Buttons extends Fragment implements View.OnTouchListener {
 
         switch (event.getAction()) {
 
-            case MotionEvent.ACTION_UP:
-                Log.d("suhel", "release " + cmd);
-                MainActivity.command.setCommand(CommandType.stop);
 
             case MotionEvent.ACTION_DOWN:
                 Log.d("suhel", "press " + cmd);
                 MainActivity.command.setCommand(cmd);
+                v.setBackgroundColor(COLOR_PRESSED);
+                break;
+
+            case MotionEvent.ACTION_UP:
+                Log.d("suhel", "release " + cmd);
+                MainActivity.command.setCommand(CommandType.stop);
+                v.setBackgroundColor(COLOR_RELEASED);
+                break;
 
         }
-
         return true;
     }
 }
--- app/src/main/java/sy/edu/au/nodemcu/WirelessCommunicator.java
+++ app/src/main/java/sy/edu/au/nodemcu/WirelessCommunicator.java
@@ -2,126 +2,12 @@ package sy.edu.au.nodemcu;
 
 import android.util.Log;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.ResponseHandlerInterface;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URI;
-import java.net.URL;
-
-import cz.msebera.android.httpclient.Header;
-import cz.msebera.android.httpclient.HttpResponse;
 
 public class WirelessCommunicator {
 
     private static String ipAddress = "http://192.168.1.103:3000/";
 
-    /*
-    final private static ResponseHandlerInterface responseHandler = new ResponseHandlerInterface() {
-        @Override
-        public void sendResponseMessage(HttpResponse response) throws IOException {
 
-        }
-
-        @Override
-        public void sendStartMessage() {
-
-        }
-
-        @Override
-        public void sendFinishMessage() {
-
-        }
-
-        @Override
-        public void sendProgressMessage(long bytesWritten, long bytesTotal) {
-
-        }
-
-        @Override
-        public void sendCancelMessage() {
-
-        }
-
-        @Override
-        public void sendSuccessMessage(int statusCode, Header[] headers, byte[] responseBody) {
-
-        }
-
-        @Override
-        public void sendFailureMessage(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
-        }
-
-        @Override
-        public void sendRetryMessage(int retryNo) {
-
-        }
-
-        @Override
-        public URI getRequestURI() {
-            return null;
-        }
-
-        @Override
-        public void setRequestURI(URI requestURI) {
-
-        }
-
-        @Override
-        public Header[] getRequestHeaders() {
-            return new Header[0];
-        }
-
-        @Override
-        public void setRequestHeaders(Header[] requestHeaders) {
-
-        }
-
-        @Override
-        public boolean getUseSynchronousMode() {
-            return false;
-        }
-
-        @Override
-        public void setUseSynchronousMode(boolean useSynchronousMode) {
-
-        }
-
-        @Override
-        public boolean getUsePoolThread() {
-            return false;
-        }
-
-        @Override
-        public void setUsePoolThread(boolean usePoolThread) {
-
-        }
-
-        @Override
-        public void onPreProcessResponse(ResponseHandlerInterface instance, HttpResponse response) {
-
-        }
-
-        @Override
-        public void onPostProcessResponse(ResponseHandlerInterface instance, HttpResponse response) {
-
-        }
-
-        @Override
-        public Object getTag() {
-            return null;
-        }
-
-        @Override
-        public void setTag(Object TAG) {
-
-        }
-    };
-    */
     public static void setIpAddress(String ipAddress) {
         WirelessCommunicator.ipAddress = ipAddress.endsWith("/") ? ipAddress : ipAddress + "/";
     }
@@ -130,17 +16,11 @@ public class WirelessCommunicator {
         return ipAddress;
     }
 
-//    private static AsyncHttpClient client = new AsyncHttpClient();
-
-//    public static void sendCommand2(String command) {
-//        client.get(ipAddress + command, responseHandler);
-//    }
 
     public static void sendCommand(String command) {
         Log.d("suhel", "sendCommand (" + command + ")");
         String url = ipAddress + command;
         new HttpGetRequest().execute(url);
-//        httpGetRequest.doInBackground(url);
     }
 
 }
